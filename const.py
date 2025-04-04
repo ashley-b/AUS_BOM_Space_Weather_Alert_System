38a5ffc19e3c9c72321db3af4e8e30a1db3c8b3d
kcoffau <35386534+kcoffau@users.noreply.github.com>
2025-04-04 14:48:02 +1100

    Create const.py

---
+++ const.py
@@ -0,0 +1,15 @@
+DOMAIN = "aus_space_weather"
+
+# List of supported locations for the K index (based on typical Australian observing sites)
+LOCATIONS = [
+    "Australian region",
+    "Hobart",
+    "Canberra",
+    "Sydney",
+    "Melbourne",
+    "Brisbane",
+    "Perth",
+    "Adelaide",
+    "Darwin",
+    "Alice Springs"
+]
